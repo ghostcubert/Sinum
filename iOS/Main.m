d37e53a3cc16ab95622093dae885eb35f46a134c
Ghostcubert <rogermarinez3@gmail.com>
2026-07-03 20:43:32 -0500

    Update API_URL to new endpoint

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"https://backend.artoog.net:3551"
+#define API_URL @"https://api.private-exal.xyz"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
